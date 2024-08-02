8d676809abf1d0aa19debf76def801e1ce24ef39
Evoiz <evoiz963@gmail.com>
2024-08-02 09:57:15 +0300

    ADD run scripts

---
+++ GUI/run.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+echo Starting GUI application...
+python GUI_pyQT.py
+echo GUI application has stopped.
+pause
---
+++ NodeJsServer/run.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+echo Starting Node.js server in development mode...
+npm run dev
+echo Server has stopped.
+pause
